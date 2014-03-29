e6391e8cd9aa0d9db4a7c5422bd489f0109ea957
Wojtek Mach <wojtek@wojtekmach.pl>
2014-03-29 00:09:35 -0400

    Add Rubyfmt.format

--- lib/rubyfmt.rb
+++ lib/rubyfmt.rb
@@ -1,5 +1,19 @@
-require "rubyfmt/version"
+require 'rubyfmt/version'
+require 'ruby2ruby'
+require 'ruby_parser'
 
 module Rubyfmt
-  # Your code goes here...
+  def self.format(string)
+    Processor.new.process(RubyParser.new.parse(string))
+  end
+
+  class Processor < Ruby2Ruby
+    def process_if(exp)
+      super.sub(/\ then$/, '')
+    end
+
+    def process_while(exp)
+      super.sub(/\ do$/, '')
+    end
+  end
 end
--- test/rubyfmt_test.rb
+++ test/rubyfmt_test.rb
@@ -1,1 +1,42 @@
 require 'minitest/autorun'
+require 'rubyfmt'
+
+describe "Rubyfmt" do
+  it do
+    assert_code %q{
+      class Person
+        def initialize(name, title = nil)
+          @name = name
+          @title = title
+        end
+        
+        def to_s
+          return "#{title} #{name}" if title
+          name
+        end
+      end
+    }, %q{
+      class Person
+
+        def initialize(name, title=nil)
+          @name=name
+          @title=title
+        end
+
+        def to_s
+          return "#{title} #{name}" if title
+          name
+        end
+
+      end
+    }
+  end
+
+  def code(str)
+    str.gsub(/^\ {6}/, '').strip
+  end
+
+  def assert_code(expected, actual)
+    assert_equal code(expected), Rubyfmt.format(code(actual))
+  end
+end
