40040e6c75f6b4113fe8e90baf7806547a6ff39a
Wojtek Mach <wojtek@wojtekmach.pl>
2014-04-06 09:50:27 -0400

    Simplify integration test

--- test/bin_rubyfmt_test.rb
+++ test/bin_rubyfmt_test.rb
@@ -2,9 +2,6 @@ require 'minitest/autorun'
 
 describe "./bin/rubyfmt" do
   it do
-    expected_path = 'test/fixtures/person.expected.rb'
-    actual_path   = 'test/fixtures/person.actual.rb'
-
-    assert_equal File.read(expected_path), `./bin/rubyfmt #{actual_path}`
+    assert_equal "foo(42)\n", `echo 'foo( 42 )' | ./bin/rubyfmt`
   end
 end
